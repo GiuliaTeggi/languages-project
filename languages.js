dfb77a2eea7c387e47d249ddb71d6e8115585f46
Giulia <teggi.giulia@gmail.com>
2017-11-25 13:31:27 +0000

    Update languages.js

--- languages.js
+++ languages.js
@@ -8,31 +8,34 @@ var nav=document.querySelector("nav");
 var header = document.querySelector("#wrapper"); 
 var words=document.querySelector(".words");
 
+//Open and close Explore list 
 explore.addEventListener("click", function() {
    if (aboutme.style.display === "flex"){
       aboutme.style.display = "none";
      main.style.display = "block";
      header.style.position = "fixed";
      nav.style.position = "fixed"; 
-     list.style.display = "290px";
+     list.style.display = "flex";
   }
-  else if (list.style.height = "0"){
+  else if (list.style.display == "none"){
     header.style.position = "fixed";
     nav.style.position = "fixed"; 
-    list.style.height = "290px";
+    list.style.display = "flex"; 
       }
-   else {list.style.height ="0";}
+   else {list.style.display ="none";}
 }); 
 
+//Close Explore list if click outside Explore link
 document.addEventListener("click", function(event){
   var click1 = explore.contains(event.target);
   if (!click1){
-  list.style.height ="0";}
+  list.style.display ="none";}
 });
 
+//Open and close About section
 about.addEventListener("click", function(){
-  if (list.style.height !== "0"){
-      list.style.height = "0";
+  if (list.style.display === "flex"){
+      list.style.display = "none";
         main.style.display = "none";
         header.style.position ="relative"; 
         nav.style.position ="relative";
@@ -49,8 +52,10 @@ about.addEventListener("click", function(){
         }
 }); 
 
+//Close About section by clicking on X icon
 close.addEventListener("click", function(){
   aboutme.style.display ="none";
   main.style.display = "block";
   nav.style.position = "fixed"; 
 });
+
