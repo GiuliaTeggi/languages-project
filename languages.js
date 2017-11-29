c6f5b84cc8681a7febef8de81f6194bc14682b54
Giulia <teggi.giulia@gmail.com>
2017-11-29 21:39:08 +0000

    Update languages.js

--- languages.js
+++ languages.js
@@ -60,13 +60,3 @@ close.addEventListener("click", function(){
   nav.style.position = "fixed"; 
 });
 
-//Slide down and up div with class "extra" by clicking on button 
-var button = document.getElementsByClassName("button");
-for(var i=0; i < button.length; i++){
-  button[i].addEventListener("click", function (){
-    var text = event.target.parentElement.querySelector(".extra");
-   text.classList.toggle("closed");
-   text.classList.toggle("open");
-  })
-}
-
